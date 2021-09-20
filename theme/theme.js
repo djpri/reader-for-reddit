9367ed67baa3e96c7d5bbc152f04ad36528334bf
djpri <22898729+djpri@users.noreply.github.com>
2021-09-20 15:16:19 +0100

    Inter font added to theme.js

--- theme/theme.js
+++ theme/theme.js
@@ -1,26 +1,55 @@
 import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 import "@fontsource/open-sans/700.css";
 import "@fontsource/open-sans/600.css";
 import "@fontsource/open-sans/400.css";
-import { mode } from "@chakra-ui/theme-tools";
+
+import "@fontsource/inter/800.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/600.css";
 
 const theme = extendTheme({
-  global: (props) => ({
-    "#__next": {
-      backgroundColor: "pink",
-      height: "100%",
-      width: "100%",
-    },
-    body: {
-      fontFamily: "body",
-      color: mode("gray.800", "whiteAlpha.900")(props),
-      bg: mode("gray.200", "gray.800")(props),
-      lineHeight: "base",
-    },
-  }),
+  styles: {
+    global: (props) => ({
+      "#__next": {
+        height: "100%",
+        width: "100%",
+      },
+      body: {
+        fontFamily: "body",
+        color: mode("gray.800", "whiteAlpha.900")(props),
+        bg: mode("gray.50", "gray.800")(props),
+        lineHeight: "base",
+      },
+    }),
+  },
   fonts: {
-    heading: "Open Sans",
-    body: "Open Sans",
+    heading: "Inter",
+    body: "Inter",
+  },
+  textStyles: {
+    heading: {
+      fontFamily: "heading",
+      textAlign: "center",
+      fontWeight: "bold",
+      letterSpacing: "-0.015em",
+      lineHeight: "1.24",
+      fontSize: { base: "2rem", md: "3.5rem" },
+    },
+    "heading-2": {
+      fontFamily: "heading",
+      textAlign: "center",
+      fontWeight: "bold",
+      letterSpacing: "-0.015em",
+      lineHeight: "1.24",
+      fontSize: { base: "1.75rem", md: "2.75rem" },
+    },
+    caps: {
+      textTransform: "uppercase",
+      fontSize: "sm",
+      letterSpacing: "widest",
+      fontWeight: "bold",
+    },
   },
 });
 
