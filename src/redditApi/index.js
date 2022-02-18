254f858e1763ad59d9f29c8b08d1491c6d1fdba4
djpri <22898729+djpri@users.noreply.github.com>
2022-02-18 01:54:52 +0000

    edited config files

--- src/redditApi/index.js
+++ src/redditApi/index.js
@@ -18,8 +18,5 @@ REDDIT.interceptors.request.use(
     }
     return config;
   },
-  function (error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
+  function (error) {}
 );
